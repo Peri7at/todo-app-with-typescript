f948c5083432b7b4277f93b47d15cca312f8ede3
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-25 11:44:20 +0300

    update app file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,35 @@
 import express from "express";
+import todoRoutes from "./routes/todos";
+import connection from "./db/config";
+import { json, urlencoded } from "body-parser";
 
 const app = express();
 
-app.listen(3000);
+app.use(json());
+
+app.use(urlencoded({ extended: true }));
+
+app.use("/todos", todoRoutes);
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
+
+connection
+  .sync()
+  .then(() => {
+    console.log("Database successfully connected");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
